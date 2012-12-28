c41e3f39d1fa080178fa0c597e73aa903f5b7f29
xmulyj <xmulyj@gmail.com>
2012-12-28 14:53:55 +0800

    disk manager

---
+++ src/chunk/DiskMgr.cpp
@@ -0,0 +1,165 @@
+/*
+ * DiskMgr.cpp
+ *
+ *  Created on: 2012-12-28
+ *      Author: LiuYongJin
+ */
+
+#include "DiskMgr.h"
+#include "slog.h"
+#include "ConfigReader.h"
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
+
+extern ConfigReader* g_config_reader;
+
+#define LOCK(lock) pthread_mutex_lock(&lock)
+#define UNLOCK(lock) pthread_mutex_unlock(&lock)
+
+void DiskMgr::make_path(string &path, string &fid, int index)
+{
+	char buf[256];
+	if(index >= 0)
+		sprintf(buf, "%s/%c%c/%02X", m_disk_path.c_str(), fid[0], fid[1], index);
+	else
+		sprintf(buf, "%s/%c%c", m_disk_path.c_str(), fid[0], fid[1]);
+	path = buf;
+}
+
+DiskMgr::DiskMgr()
+{
+	m_disk_path = g_config_reader->GetValueString("DiskDir", "/tmp/sfs_chunk");  //数据存放路径
+}
+
+void DiskMgr::init()
+{
+	int i;
+	char pre_fix[3];
+	struct stat path_stat;
+
+	//目录不存在
+	if(stat(m_disk_path.c_str(), &path_stat)==-1 && errno==ENOENT)
+	{
+		int result = mkdir(m_disk_path.c_str(), S_IRWXU);
+		assert(result == 0);
+	}
+
+	//加载00,01,...,FF  256个子目录
+	for(i=0; i<DIR_NUM; ++i)
+	{
+		int index = 0;
+		sprintf(pre_fix, "%02X", i);
+		m_disk_files[i].pre_fix = pre_fix;
+		m_disk_files[i].fp = NULL;
+		m_disk_files[i].index = -1;
+		m_disk_files[i].cur_pos = 0;
+		pthread_mutex_init(&m_disk_files[i].lock, NULL);
+
+		string sub_dir = m_disk_path+"/"+pre_fix;
+		SLOG_DEBUG("checking chunk_dir:%s.", sub_dir.c_str());
+		if(stat(sub_dir.c_str(), &path_stat)==-1 && errno==ENOENT) //子目录不存在
+		{
+			SLOG_INFO("sub dir[%s] not exists. create it.", sub_dir.c_str());
+			if(mkdir(sub_dir.c_str(), S_IRWXU) == -1)
+			{
+				SLOG_ERROR("make dir failed.sub_dir=%s, error:%s", sub_dir.c_str(), strerror(errno));
+				continue;
+			}
+		}
+		else if(!S_ISDIR(path_stat.st_mode)) //不是目录
+		{
+			SLOG_ERROR("not dir.sub_dir=%s.", sub_dir.c_str());
+			continue;
+		}
+
+		//open file
+		struct dirent* ent = NULL;
+		DIR *dir;
+		if((dir=opendir(sub_dir.c_str())) == NULL)
+		{
+			SLOG_ERROR("open dir error. sub_dir=%s, error:%s", sub_dir.c_str(), strerror(errno));
+			continue;
+		}
+		while((ent=readdir(dir)) != NULL)  //计算文件数
+		{
+			if(strcmp( ".",ent->d_name) == 0 || strcmp( "..",ent->d_name) == 0)
+				continue;
+			++index;
+		}
+		closedir(dir);
+		if(index > 0)  //最后一个文件
+			--index;
+
+		char name[256];
+		sprintf(name, "%s/%02X", sub_dir.c_str(), index);
+		m_disk_files[i].fp= fopen(name, "w");
+		assert(m_disk_files[i].fp != NULL);
+		fseek(m_disk_files[i].fp, 0L, SEEK_END);
+		m_disk_files[i].cur_pos = ftell(m_disk_files[i].fp);
+		m_disk_files[i].index = index;
+
+		SLOG_DEBUG("load file succ. name=%s, size=%d.", name, m_disk_files[i].cur_pos);
+	}
+}
+
+void DiskMgr::uninit()
+{
+	int i;
+	for(i=0; i<DIR_NUM; ++i)
+	{
+		if(m_disk_files[i].fp != NULL)
+			fclose(m_disk_files[i].fp);
+		pthread_mutex_destroy(&m_disk_files[i].lock);
+	}
+}
+
+bool DiskMgr::save_file_to_disk(string &fid, char *buf, uint32_t size, ChunkPath &chunkpath)
+{
+	const char *temp = fid.c_str();
+	int i=0, index= 0;
+	for(; i<2; ++i)  //get index
+	{
+		index *= 16;
+		if(temp[i]>='A'&&temp[i]<='F')
+			index += temp[i]-'A';
+		else if(temp[i]>='a'&&temp[i]<='f')
+			index += temp[i]-'a';
+		else
+			return false;
+	}
+	assert(index>=0 && index<DIR_NUM);
+	DiskFile &disk_file = m_disk_files[index];
+	assert(disk_file.fp != NULL);
+
+	LOCK(disk_file.lock);
+
+	if(disk_file.cur_pos+size > MAX_FLESIZE)  //重新打开文件
+	{
+		fclose(disk_file.fp);
+		string path;
+		make_path(path, fid, ++disk_file.index);
+		disk_file.fp = fopen(path.c_str(), "w");
+		assert(disk_file.fp != NULL);
+		disk_file.cur_pos = 0;
+	}
+
+	chunkpath.id = g_config_reader->GetValueString("ChunkID");
+	assert(chunkpath.id != "");
+	chunkpath.addr = g_config_reader->GetValueString("ChunkIP");
+	assert(chunkpath.addr != "");
+	chunkpath.port = g_config_reader->GetValueInt("ChunkPort", 3013);
+	chunkpath.index = disk_file.index;
+	chunkpath.offset = disk_file.cur_pos;
+
+	fwrite(buf, 1, size, disk_file.fp);
+	disk_file.cur_pos += size;
+
+	UNLOCK(disk_file.lock);
+	return true;
+}
---
+++ src/chunk/DiskMgr.h
@@ -0,0 +1,57 @@
+/*
+ * DiskMgr.h
+ *
+ *  Created on: 2012-12-28
+ *      Author: LiuYongJin
+ */
+
+#ifndef _DISK_MANAGER_H_
+#define _DISK_MANAGER_H_
+
+#include <stdint.h>
+#include <pthread.h>
+#include <string>
+using std::string;
+
+#include "SFSProtocolFamily.h"
+
+#define DIR_NUM 256
+#define MAX_FLESIZE 1024*1024*1024  //1G
+
+typedef struct _disk_file
+{
+	string pre_fix;
+	FILE *fp;
+	int index;
+	uint32_t cur_pos;
+	pthread_mutex_t lock;
+}DiskFile;
+
+class DiskMgr
+{
+public:
+	static DiskMgr* get_instance()
+	{
+		static DiskMgr* g_disk_mgr = NULL;
+		if(g_disk_mgr == NULL)
+			g_disk_mgr = new DiskMgr;
+		return g_disk_mgr;
+	}
+
+	//初始化,加载磁盘文件
+	void init();
+	void uninit();
+
+	//将fid的size字节的数据buf保存到磁盘,返回chunk_path;成功返回true,失败返回false
+	bool save_file_to_disk(string &fid, char *buf, uint32_t size, ChunkPath &chunkpath);
+private:
+	DiskMgr();
+	void make_path(string &path, string &fid, int index);
+private:
+	string m_disk_path;
+	DiskFile m_disk_files[DIR_NUM];
+};
+
+#endif //_DISK_MANAGER_H_
+
+
